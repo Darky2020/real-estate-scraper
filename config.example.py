bfedd024403b3133671cb9f704d5487afbf11b17
Yaroslaw Biloshytskyi <yaroslaw0upwork@gmail.com>
2023-06-22 22:55:29 +0300

    Init

---
+++ config.example.py
@@ -0,0 +1,11 @@
+ENDPOINT = "https://www.real-estate.lviv.ua/<settings>"
+
+# Coordinates
+BASE_LAT = 10
+BASE_LNG = 20
+MAX_DISTANCE = 1.75 # km
+
+MIN_PRICE = 4000 # UAH
+
+BOT_TOKEN = "TOKEN"
+TELEGRAM_ID = 0
